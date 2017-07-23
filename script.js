018a52cccf724d128801eef18145bbefaa126a64
tomng1118 <chishingng@chideMacBook-Pro.local>
2017-07-23 20:58:05 +0800

    nav
    Video

--- script.js
+++ script.js
@@ -11,16 +11,6 @@ $('.work_item').mouseout(function() {
 
 
 
-
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 100) {
-    $('.nav-desk').addClass('scrolled');
-  };
-  if ($(window).scrollTop() < 100) {
-    $('.nav-desk').removeClass('scrolled');
-  }
-});
-
 if ($(window).width() <= 768) {
   $(".work_item").children('.tag').addClass('hover');
   $(".work_item").children('.overlay').addClass('hover');
