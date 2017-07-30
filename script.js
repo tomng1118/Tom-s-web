6fa384ec05b33756f1f625d34397e37e51ad08db
tomng1118 <a877418388@gmail.com>
2017-07-31 01:11:22 +0800

    click to scroll effect

--- script.js
+++ script.js
@@ -16,3 +16,31 @@ if ($(window).width() <= 768) {
   $(".work_item").children('.overlay').addClass('hover');
   $("#logo").attr('src', 'logo-mobile.png');
 };
+
+$("#nav-home").click(function() {
+  $('html,body').animate({
+      scrollTop: $(".hero").offset().top
+    },
+    'slow');
+});
+
+$("#nav-about").click(function() {
+  $('html,body').animate({
+      scrollTop: $(".intro").offset().top
+    },
+    'slow');
+});
+
+$("#nav-works").click(function() {
+  $('html,body').animate({
+      scrollTop: $(".works").offset().top
+    },
+    'slow');
+});
+
+$("#nav-contact").click(function() {
+  $('html,body').animate({
+      scrollTop: $(".about").offset().top
+    },
+    'slow');
+});
