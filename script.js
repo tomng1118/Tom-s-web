5addc5c13472efdf1238acdca5e182cfc9106cb6
tomng1118 <a877418388@gmail.com>
2017-07-31 00:46:58 +0800

    Revert "A"
    This reverts commit c39cb1007cc0fc6e7ae78e0ec4fb1a0ecff40964.

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 //hover show tag functions
 
-
-if ($(window).width() >= 768) {
-  $('.work_item').mouseover(function() {
-    $(this).children('.tag').addClass('hover');
-    $(this).children('.overlay').addClass('hover');
-  });
-  $('.work_item').mouseout(function() {
-    $(this).children('.tag').removeClass('hover');
-    $(this).children('.overlay').removeClass('hover');
-  });
-};
-
 $('.work_item').mouseover(function() {
   $(this).children('.tag').addClass('hover');
   $(this).children('.overlay').addClass('hover');
@@ -26,8 +14,5 @@ $('.work_item').mouseout(function() {
 if ($(window).width() <= 768) {
   $(".work_item").children('.tag').addClass('hover');
   $(".work_item").children('.overlay').addClass('hover');
+  $("#logo").attr('src', 'logo-mobile.png');
 };
-
-$("#scroll").click(function() {
-
-});
